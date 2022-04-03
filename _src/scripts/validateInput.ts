daf5f3bf6e00f0e5481e0e420e6d180323a0fdf5
Jamie Evawin <iamjevawin+gh@gmail.com>
2022-04-03 15:54:49 +0100

    💄 Alex's new style - incomplete
    Co-authored-by: Alex Clapperton <Pushedskydiver@users.noreply.github.com>

--- _src/scripts/validateInput.ts
+++ _src/scripts/validateInput.ts
@@ -2,32 +2,36 @@ import updateResultsCount from "./updateResultsCount";
 
 function ValidateInput(domain: HTMLInputElement, query: HTMLTextAreaElement) {
   const resultsTable = document.querySelector("[data-results-table]");
-  const resultsCount = document.querySelector('[data-results-count]');
+  const resultsCount = document.querySelector("[data-results-count]");
   const button = document.querySelector('[type="submit"]');
   const endpoint = "https://search-the-source-code.herokuapp.com/";
-  const urlRegex = /^(http(s)?:\/\/)?(www\.)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/;
+  const urlRegex =
+    /^(http(s)?:\/\/)?(www\.)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/;
+  const errorClasses = ["bg-pink-700/[0.15]", "border-pink-700/[0.2]"];
+  const successClasses = ["bg-lime-700/[0.15]", "border-lime-700/[0.2]"];
+  const defaultClasses = ["bg-zinc-900/[0.15]", "border-zinc-900/[0.075]"];
   let inputValidationErrorFound = false;
 
   function setInvalidInputState(input: HTMLInputElement | HTMLTextAreaElement) {
     const inputId = input.id;
 
-    input.setAttribute('aria-describedby', `${inputId}Describe`);
-    input.classList.add('border-red-500');
-    input.classList.remove('border-gray-300');
-    input.classList.remove('border-lime-500');
-    input.nextElementSibling?.removeAttribute('hidden');
+    input.setAttribute("aria-describedby", `${inputId}Describe`);
+    input.classList.add(...errorClasses);
+    input.classList.remove(...defaultClasses);
+    input.classList.remove(...successClasses);
+    input.nextElementSibling?.classList.remove("invisible");
   }
 
   function setValidInputState(input: HTMLInputElement | HTMLTextAreaElement) {
-    input.removeAttribute('aria-describedby');
-    input.classList.remove('border-red-500');
-    input.classList.remove('border-gray-300');
-    input.classList.add('border-lime-500');
-    input.nextElementSibling?.setAttribute('hidden', 'true');
+    input.removeAttribute("aria-describedby");
+    input.classList.remove(...errorClasses);
+    input.classList.remove(...defaultClasses);
+    input.classList.add(...successClasses);
+    input.nextElementSibling?.classList.add("invisible");
   }
 
   function validateInput(input: HTMLInputElement | HTMLTextAreaElement) {
-    const isUrl = input.getAttribute('type') === 'url';
+    const isUrl = input.getAttribute("type") === "url";
     const isValidUrl = urlRegex.test(input.value);
     const isValidInput = isUrl ? isValidUrl : input.value.length > 0;
 
@@ -62,14 +66,14 @@ function ValidateInput(domain: HTMLInputElement, query: HTMLTextAreaElement) {
       fetch(url)
         .then((response) => response.json())
         .then((data) => {
-          import('./showResults').then(module => module.default(data).init());
+          import("./showResults").then((module) => module.default(data).init());
         });
     }
   }
 
   return {
-    init
-  }
+    init,
+  };
 }
 
 export default ValidateInput;
