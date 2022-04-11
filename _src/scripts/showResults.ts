dc7735a4c506fca1d0330058a1473b1fef5d0d03
Alex Clapperton <hi@alexclapperton.co.uk>
2022-04-11 21:06:31 +0100

    ♻️ Tidy up how results tables rendered, add syntax highlights to snippets

--- _src/scripts/showResults.ts
+++ _src/scripts/showResults.ts
@@ -1,6 +1,6 @@
 import updateResultsCount from "./updateResultsCount";
 
-type ResultData = {
+export type ResultData = {
   url: string,
   hostless: string,
   matches: string,
@@ -9,105 +9,15 @@ type ResultData = {
 }
 
 function ShowResults(results: ResultData[]) {
-  const holder = document.querySelector("[data-results-table]");
-  const button = document.querySelector('[type="submit"]');
-
-  function renderMobileTable() {
-    let output = "";
-
-    results.forEach((result) => {
-      if (result.count > 0) {
-        output += `
-        <div class="relative overflow-x-auto rounded border border-gray-200 max-w-5xl mx-auto mb-4">
-          <table class="w-full text-sm text-left text-gray-500">
-            <tbody>
-              <tr class="bg-white border-b border-gray-200">
-                <th class="p-2 text-xs text-gray-700 uppercase bg-gray-100">Matches</th>
-                <td class="p-2">${result.count}</td>
-              </tr>
-              <tr class="bg-white border-b border-gray-200">
-                <th class="p-2 text-xs text-gray-700 uppercase bg-gray-100">URL</th>
-                <td class="p-2">
-                  <a href="${result.url}" target="_blank" class="text-blue-600">${result.hostless}</a>
-                </td>
-              </tr>
-              <tr class="bg-white border-b border-gray-200">
-                <th class="p-2 text-xs text-gray-700 uppercase bg-gray-100">Snippet</th>
-                <td class="p-2">
-                  <code class="block whitespace-normal break-all">
-                    ${result.snippet.toString().replace(/</g, "&lt;").replace(/>/g, "&gt;")}}
-                  </code>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-        `;
-      }
-    });
-
-
-    if (holder) {
-      holder.innerHTML = output;
-    }
-  }
-
-  function renderDesktopTable() {
-    let output = "";
-
-    results.forEach((result: ResultData) => {
-      if (result.count > 0) {
-        output += `
-          <tr class="bg-white border-b border-gray-200">
-            <td class="p-4">${result.count}</td>
-            <td class="p-4">
-              <a href="${result.url}" target="_blank" class="text-blue-600">${result.hostless}</a>
-            </td>
-            <td class="p-4">
-              <code class="block whitespace-normal break-all">
-                ${result.snippet.toString().replace(/</g, "&lt;").replace(/>/g, "&gt;")}}
-              </code>
-            </td>
-          </tr>
-        `;
-      }
-    });
-
-    if (holder) {
-      holder.innerHTML = `
-        <div class="relative overflow-x-auto rounded border border-gray-200 max-w-5xl mx-auto">
-          <table class="w-full text-sm text-left text-gray-500">
-            <thead class="text-xs text-gray-700 uppercase bg-gray-100">
-              <tr>
-                <th class="p-4">Matches</th>
-                <th class="p-4">URL</th>
-                <th class="p-4">Snippet</th>
-              </tr>
-            </thead>
-
-            <tbody>
-              ${output}
-            </tbody>
-          </table>
-        </div>
-      `;
-    }
-  }
-
   function init() {
-    const hasNoResults = results.every(result => result.count === 0);
+    const resultsActual = results.filter(result => result.count > 0);
+    const resultsCount = resultsActual.length;
 
-    if (button) {
-      const resultsLength = results.filter(result => result.count > 0).length;
-      button.classList.remove("pointer-events-none", "opacity-50");
-      updateResultsCount(resultsLength);
-    }
+    if (resultsCount === 0) return updateResultsCount(0);
 
-    if (hasNoResults) return updateResultsCount(0);
+    import("./renderResultsTable").then((module) => module.default(resultsActual).init());
 
-    if (window.innerWidth >= 500) return renderDesktopTable();
-
-    return renderMobileTable();
+    return updateResultsCount(resultsCount);
   }
 
   return {
--- _src/scripts/validateInput.ts
+++ _src/scripts/validateInput.ts
@@ -5,13 +5,12 @@ function ValidateInput(domain: HTMLInputElement, query: HTMLTextAreaElement) {
   const resultsCount = document.querySelector("[data-results-count]");
   const button = document.querySelector('[type="submit"]');
   const endpoint = "https://search-the-source-code.herokuapp.com/";
-  const urlRegex =
-    /^(http(s)?:\/\/)?(www\.)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/;
-  let inputValidationErrorFound = false;
+  const urlRegex = /^(http(s)?:\/\/)?(www\.)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/;
   const errorClasses = ["text-red-800"];
   const validClasses = ["text-green-900"];
   const check = `<svg class="fill-green-900 inline" width="18" height="18" viewBox="0 0 693 693" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M672.4 113.4a48.5 48.5 0 0 0-69 0L242 475.4 90 323a49.6 49.6 0 0 0-68.9 71.3l186.4 186.3a48.5 48.5 0 0 0 68.9 0l396-396a48.5 48.5 0 0 0 0-71.2Z"/></svg>`;
   const cross = `<svg class="fill-red-800 inline" width="16" height="16" viewBox="0 0 693 693" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M185 114.4a50 50 0 1 0-70.6 70.7l162.2 162.2-162.2 162.3a50 50 0 0 0 70.7 70.7l162.2-162.2 162.3 162.2a50 50 0 0 0 70.7-70.7L418.1 347.3l162.2-162.2a50 50 0 0 0-70.7-70.7L347.3 276.6 185.1 114.4Z"/></svg>`;
+  let inputValidationErrorFound = false;
 
   function setInvalidInputState(input: HTMLInputElement | HTMLTextAreaElement) {
     const inputId = input.id;
@@ -25,7 +24,7 @@ function ValidateInput(domain: HTMLInputElement, query: HTMLTextAreaElement) {
     // error message
     if (vIcon) vIcon.innerHTML = cross;
     if (vMessage) vMessage.innerHTML = `${inputId === "domain" ? "Domain" : "Code"} doesn't quite look right.`
-    
+
     input.nextElementSibling?.classList.remove("invisible");
 
     input.setAttribute("aria-describedby", `${inputId}Describe`);
@@ -35,7 +34,7 @@ function ValidateInput(domain: HTMLInputElement, query: HTMLTextAreaElement) {
     const inputId = input.id;
     const vIcon = input.nextElementSibling?.querySelector("[data-validation-icon]") || false;
     const vMessage = input.nextElementSibling?.querySelector("[data-validation-message]") || false;
-    
+
     // valid state
     input.nextElementSibling?.classList.remove(...errorClasses);
     input.nextElementSibling?.classList.add(...validClasses);
@@ -68,32 +67,38 @@ function ValidateInput(domain: HTMLInputElement, query: HTMLTextAreaElement) {
     return inputValidationErrorFound;
   }
 
+  function enableSubmitButton() {
+    button?.classList.remove("pointer-events-none", "opacity-50");
+  }
+
   function init() {
     const url = `${endpoint}?domain=${domain.value}&search=${encodeURIComponent(query.value)}`;
 
     validateInput(domain);
     validateInput(query);
 
-    if (!inputValidationErrorFound) {
-      if (button && resultsCount) {
-        button.classList.add("pointer-events-none", "opacity-50");
+    if (inputValidationErrorFound) return;
 
-        updateResultsCount(-1);
+    if (resultsCount) {
+      button?.classList.add("pointer-events-none", "opacity-50");
 
-        if (resultsTable) resultsTable.textContent = "";
-      }
+      updateResultsCount(-1);
 
-      fetch(url)
-        .then((response) => response.json())
-        .then((data) => {
-          import("./showResults").then((module) => module.default(data).init());
-        })
-        .catch((error) => {
-          updateResultsCount(-2);
-
-          console.error(error);
-        });
+      if (resultsTable) resultsTable.textContent = "";
     }
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        import("./showResults").then(module => module.default(data).init());
+        enableSubmitButton();
+      })
+      .catch((error) => {
+        updateResultsCount(-2);
+        enableSubmitButton();
+
+        console.error(error);
+      });
   }
 
   return {
